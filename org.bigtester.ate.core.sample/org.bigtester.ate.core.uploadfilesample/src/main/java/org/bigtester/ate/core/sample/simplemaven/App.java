5759268817280bafd937d26163fc1d6e702af6a4
Jun-Yang <wpbxwpbx@gmail.com>
2015-04-30 16:14:30 -0300

    Upload File Sample

--- org.bigtester.ate.core.sample/org.bigtester.ate.core.uploadfilesample/src/main/java/org/bigtester/ate/core/sample/simplemaven/App.java
+++ org.bigtester.ate.core.sample/org.bigtester.ate.core.uploadfilesample/src/main/java/org/bigtester/ate/core/sample/simplemaven/App.java
@@ -1,10 +1,13 @@
 package org.bigtester.ate.core.sample.simplemaven;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.sql.SQLException;
 
 import org.bigtester.ate.TestProjectRunner;
 import org.dbunit.DatabaseUnitException;
+
+import com.github.javaparser.ParseException;
 
 /**
  * Hello world!
@@ -14,6 +17,17 @@ public class App
 {
     public static void main( String[] args ) throws MalformedURLException, DatabaseUnitException, SQLException
     {
-    	TestProjectRunner.runTest("testproject/testproject.xml");
+    	try {
+			TestProjectRunner.runTest("bigtesterTest/testproject.xml");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
